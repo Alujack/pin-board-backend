348e3b6fee96b598ed9a2b99f3a0ab3189fc4b21
dynann <dynannran@gmail.com>
2025-10-01 22:22:38 +0700

    Add JWT authentication and role-based access control; update user types and remove error handler

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,6 +1,13 @@
+import { RoleEnum } from "./enums"
+import { Request } from "express"
 export interface IUser {
     id: string
     username: string
     email: string
     passwrod: string
+    role: RoleEnum
+}
+
+export interface IRequest extends Request {
+    user?: IUser
 }
